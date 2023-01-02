c2fce6c698d9d030c92a5048ffe73c2b7c3fcfc3
Svetlana Ponomareva <svetlana.andreyevna@gmail.com>
2023-01-02 23:46:19 +0200

    sem 1

--- Python_Sem_1_13-12/Sem1_HomeWork/HM1_task2_predicaty.py
+++ Python_Sem_1_13-12/Sem1_HomeWork/HM1_task2_predicaty.py
@@ -1,6 +1,23 @@
 # Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
 
-if not (x or y or z) == (not x and not y and not z):
-    print(True)
-else:
-    print(False)
+# not (x or y or z) == (not x and not y and not z):
+
+for x in [True, False]:
+    for y in [True, False]:
+        for z in [True, False]:
+            if not (x or y or z) == (not x and not y and not z):
+                print(f'Выражение при x ={x}, y={y}, z={z} верно')
+
+# x y z 
+
+# t t t
+# t t f
+# t f t
+# t f f 
+
+# f t t 
+# f t f 
+# f f t 
+# f f t
+
+
